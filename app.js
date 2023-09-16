c5c897a9b62a0e0a8b1bdb4e98ae5acb6af9218b
jennifer cox <jenncox97@gmail.comgit config --global user.email jenncox97@gmail.com>
2023-09-15 22:31:52 -0400

    completed

--- app.js
+++ app.js
@@ -3,8 +3,29 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName= "Vash";
+let lastName= "Stampede";
+var age= 25; 
 
-// Exercise 2
+lastName="Saverem"
+age=150;
 
-// Exercise 3
+let language="JavaScript";
+let createdYear=1995;
+let isCaseSensitive="true";
+console.log(`${language} was first released in ${createdYear}.`);
+
+let price=19.99;
+let isOnSale="false";
+let salePercentage=15;
+let stock=0;
+let inStock="false";
+let selectedSize="M";
+console.log(`${selectedSize} is in stock ${inStock}`);
+
+let title="Name of the Wind";
+let author="Patrick Rothfuss";
+let pageCount=722;
+let bookmark=456;
+let hasRead="true";
+console.log(`I have read ${title} by ${author}? ${hasRead}`);
